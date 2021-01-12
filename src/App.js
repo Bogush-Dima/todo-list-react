f0e0e1793df941da3a90a77a4edcc2b32efcfb22
Bogush-Dima <68421669+Bogush-Dima@users.noreply.github.com>
2021-01-12 10:39:26 +0300

    feat: create base structure app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const Header = () => {
+  return <h1 className="title">TODO</h1>;
+};
+
+const TodoItem = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo-item'>
+      <input className="todo-item__check" type="checkbox" />
+      <span className='todo-item__title'>Task-1</span>
     </div>
   );
+};
+
+const TodoList = () => {
+  return (
+    <section className="todos">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </section>
+  );
+};
+
+const NewTaskText = () => {
+  return (
+    <input className='new-task' type='text' placeholder='Add Task' />
+  )
 }
+
+const AddButton = () => {
+  return (
+    <button className='button add-button'>
+      add
+    </button>
+  )
+}
+
+const AddTask = () => {
+  return (
+    <div className='add-task'>
+      <NewTaskText />
+      <AddButton />
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <TodoList />
+      <AddTask/>
+    </div>
+  );
+};
 
 export default App;
