e2035049b2e140ea16a810745dbaf0729ec11fa4
Bogush-Dima <68421669+Bogush-Dima@users.noreply.github.com>
2021-01-15 15:55:27 +0300

    asdsad

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,16 +2,17 @@ import styles from "./SideBar.module.css";
 
 const SideBar = () => {
   return (
-    <nav className={styles.navBar}>
-      <ul className={styles.list}>
-        <li className={styles.listItem}>
-          <a className={styles.link} href='#'>Work</a>
-        </li>
-        <li className={styles.listItem}>
-          <a className={styles.link} href='#'>Private</a>
-        </li>
-      </ul>
-    </nav>
+    // <nav className={styles.navBar}>
+    //   <ul className={styles.list}>
+    //     <li className={styles.listItem}>
+    //       <a className={styles.link} href='#'>Work</a>
+    //     </li>
+    //     <li className={styles.listItem}>
+    //       <a className={styles.link} href='#'>Private</a>
+    //     </li>
+    //   </ul>
+    // </nav>
+    <p>asda</p>
   );
 };
 
