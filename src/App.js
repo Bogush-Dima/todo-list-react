9ba8229b97c387dfec68605ad396cedab47947bc
Bogush-Dima <68421669+Bogush-Dima@users.noreply.github.com>
2021-01-16 23:21:53 +0300

    feat: update savetodos to localstorage with useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import styles from "./App.module.css";
 import Footer from "./components/Footer/Footer";
@@ -8,43 +8,12 @@ import SideBar from "./components/SideBar/SideBar";
 import TodoList from "./components/TodoList/TodoList";
 
 const App = () => {
-  const storageTodos = JSON.parse(localStorage.getItem("todos"))
-  const [todos, setTodos] = useState(storageTodos || [])
-    // if(storageTodos.isArray()) {
-    //   return storageTodos
-    // } else {
-    //   return []
-    // }
-    // JSON.parse(localStorage.getItem("todos"))
-    //   [
-    //   {
-    //     id: 0,
-    //     completed: false,
-    //     category: "work",
-    //     title: "Work-1",
-    //   },
-    //   {
-    //     id: 1,
-    //     completed: false,
-    //     category: "work",
-    //     title: "Work-2",
-    //   },
-    //   {
-    //     id: 2,
-    //     completed: false,
-    //     category: "private",
-    //     title: "Private-1",
-    //   },
-    //   {
-    //     id: 3,
-    //     completed: false,
-    //     category: "private",
-    //     title: "Private-2",
-    //   },
-    // ]
-  // );
+  const storageTodos = JSON.parse(localStorage.getItem("todos"));
+  const [todos, setTodos] = useState(storageTodos || []);
 
-  localStorage.setItem("todos", JSON.stringify(todos));
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const getTodosByCategory = (category) =>
     todos.filter((todo) => todo.category === category);
