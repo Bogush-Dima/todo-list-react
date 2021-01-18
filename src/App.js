c1d0899fc9697c40c982ebe513f5c04c03538032
Bogush-Dima <68421669+Bogush-Dima@users.noreply.github.com>
2021-01-18 10:19:53 +0300

    feat: change path to components, change route structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import styles from "./App.module.css";
-import Main from "./components/Main/Main";
-import SideBar from "./components/SideBar/SideBar";
-import TodoList from "./components/TodoList/TodoList";
+import styles from "App.module.css";
+import Main from "components/Main/Main";
+import SideBar from "components/SideBar/SideBar";
+import TodoList from "components/TodoList/TodoList";
 
 const App = () => {
   const storageTodos = JSON.parse(localStorage.getItem("todos"));
@@ -24,22 +24,32 @@ const App = () => {
           <Route exact path="/">
             <Main />
           </Route>
-          <Route path="/work">
-            <TodoList
-              title="WORK"
-              todos={getTodosByCategory("work")}
-              setTodos={setTodos}
-              allTodos={todos}
-            />
-          </Route>
-          <Route path="/private">
-            <TodoList
-              title="PRIVATE"
-              todos={getTodosByCategory("private")}
-              setTodos={setTodos}
-              allTodos={todos}
-            />
-          </Route>
+          <Route
+            path="/work"
+            render={() => {
+              return (
+                <TodoList
+                  title="WORK"
+                  todos={getTodosByCategory("work")}
+                  setTodos={setTodos}
+                  allTodos={todos}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/private"
+            render={() => {
+              return (
+                <TodoList
+                  title="PRIVATE"
+                  todos={getTodosByCategory("private")}
+                  setTodos={setTodos}
+                  allTodos={todos}
+                />
+              );
+            }}
+          />
         </div>
       </div>
     </BrowserRouter>
